fbb8be8f9b42b6207ffcfa64ac0e4d3886bad66f
zarif <zarif@icloud.com>
2024-09-28 18:59:16 +0500

    test-commit

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -12,21 +12,23 @@ export default function SimpleSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    fade: true
+    fade: true,
+    arrows: false
   };
+
   return (
-    <div className='relative'>
-      <Slider {...settings} className="w-full">
+    <div className='relative w-full'>
+      <Slider {...settings} className="w-full h-screen">
         {DATES.map((item) => (
-          <div key={item.id} className="h-screen w-full">
-            <img className="w-full h-full" src={item.img} alt="" />
+          <div key={item.id} className="relative h-screen w-full">
+            <img className="w-full h-full object-cover" src={item.img} alt="" />
+            <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex flex-col justify-center items-center">
+              <h1 className="text-white text-xl mb-20 font-vibes">Приглашение</h1> 
+              <p className="text-white text-lg font-vibes leading-none text-center"> Aзизжон  <br />&<br /> Наргиза</p>
+            </div>
           </div>
         ))}
       </Slider>
-      <div className="absolute top-0 h-full bg-gray-900 bg-opacity-50 w-full flex flex-col justify-center items-center">
-        <h1 className="text-white text-xl mb-20 font-vibes">Приглашение</h1> 
-        <p className="text-white text-lg font-vibes leading-none text-center"> Aзизжон  <br />&<br /> Наргиза</p>
-      </div>
     </div>
   );
 }
